d3650e627473f651c2d4a2dcf6d7c0a1741155cd
Hrldlpnjr <haroldalpino5015@gmail.com>
2025-09-28 13:51:39 +0800

    Display of the game

--- DragRacePHGuessingGame/Program.cs
+++ DragRacePHGuessingGame/Program.cs
@@ -4,8 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, Try!");
+
+            Console.WriteLine("🌟 DRAG RACE PHILIPPINES QUOTETIVITY GUESSING GAME 🌟");
+            Console.WriteLine("Choose your Season:");
+            Console.WriteLine("1. Season 1 - AVAILABLE ✨");
+            Console.WriteLine("2. Season 2 - HINDI PA NAGAGAWA TEH 🚧");
+            Console.WriteLine("3. Season 3 - HINDI PA RIN NGA NAGAGAWA 🚧");
+            Console.WriteLine("4. Exit");
+            Console.WriteLine();
+            Console.ReadKey();
 
         }
     }
