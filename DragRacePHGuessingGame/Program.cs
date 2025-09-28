5ba9da2a987689741f947459992532f53a67e4be
Hrldlpnjr <haroldalpino5015@gmail.com>
2025-09-28 14:04:33 +0800

    Getting the user input

--- DragRacePHGuessingGame/Program.cs
+++ DragRacePHGuessingGame/Program.cs
@@ -10,8 +10,11 @@
             Console.WriteLine("1. Season 1 - AVAILABLE ✨");
             Console.WriteLine("2. Season 2 - HINDI PA NAGAGAWA TEH 🚧");
             Console.WriteLine("3. Season 3 - HINDI PA RIN NGA NAGAGAWA 🚧");
-            Console.WriteLine("4. Exit");  
+            Console.WriteLine("4. Exit");
+            Console.WriteLine("Choose: ");
 
+            string userInput = Console.ReadLine();
+            Console.WriteLine("You chose: " + userInput);
         }
     }
 }
